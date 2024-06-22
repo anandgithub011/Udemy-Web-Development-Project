58d96bd34cb7bf9ab495430c6d25c2733ea001c1
anandgithub011 <anandany830@gmail.com>
2024-06-23 01:23:05 +0530

    Section-15

---
+++ Section-15/101/index.js
@@ -0,0 +1,11 @@
+prompt("Male Name");
+prompt("Female Nme");
+
+function showPercentage(){
+    var n = Math.random();
+    n = n *100;
+    n = Math.floor(n) + 1;
+    return n;
+}
+
+showPercentage();
